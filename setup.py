ffd50a2657403e0be313a9082d2a7ac7c7047080
SeanSRice <37247379+SeanSRice@users.noreply.github.com>
2019-07-18 16:19:08 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='moduleofdoom',
+      version='0.1',
+      description='Just trying to make a module',
+      url='http://github.com/storborg/funniest',
+      author='SR',
+      author_email='nonya@business.com',
+      license='MIT',
+      packages=['moduleofdoom'],
+      zip_safe=False)
